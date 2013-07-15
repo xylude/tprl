29ae215269f37d730ead1cbde73357cae634eb96
Zane <beastsmc@gmail.com>
2013-07-14 16:34:36 -1000

    Added safe teleporting.

--- src/com/craftmoar/tprl/Tprl.java
+++ src/com/craftmoar/tprl/Tprl.java
@@ -3,6 +3,7 @@ package com.craftmoar.tprl;
 import java.util.Random;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -36,15 +37,16 @@ public class Tprl extends JavaPlugin {
 						"min_default_location"));
 				int default_max = Integer.parseInt(this.getConfig().getString(
 						"max_default_location"));
-				Location loc = this.getRandomLoc(default_min, default_max, default_world);
+				Location loc = this.getRandomLoc(default_min, default_max, default_world, 0);
 				player.setNoDamageTicks(ndt);
 				player.teleport(loc);
 			} else if (args.length == 2) {
 				// send to a random location in the specified params
 				int default_min = Integer.parseInt(args[0]);
 				int default_max = Integer.parseInt(args[1]);
-				Location loc = this.getRandomLoc(default_min, default_max, default_world);
+				Location loc = this.getRandomLoc(default_min, default_max, default_world, 0);
 				player.setNoDamageTicks(ndt);
+				
 				player.teleport(loc);
 			} else {
 				sender.sendMessage("Usage: /tprl [loc1] [loc2] or /tprl (for totally random)");
@@ -54,15 +56,26 @@ public class Tprl extends JavaPlugin {
 		return true;
 	}
 
-	private Location getRandomLoc(int min, int max, World world) {
-		Location loc = new Location(world, this.randomNum(min, max), 255,
-				this.randomNum(min, max));
-		return loc;
+	private Location getRandomLoc(int min, int max, World world, int attempts) {
+		if(attempts>5) {
+			return null;
+		}
+		int x = randomNum(min, max);
+		int height = 140;
+		int z = randomNum(min, max);
+		Location loc = new Location(world, x, height, z);
+		while(loc.getBlock().getType()==Material.AIR) {
+			if(height<1) {
+				//Looks like this entire column was air?
+				getRandomLoc(min, max, world, attempts++);
+			}
+			height--;
+			loc = new Location(world, x, height, z);
+		}
+		return new Location(world, x, height+1, z);
 	}
-
 	private int randomNum(Integer lownum, Integer highnum) {
 		Random rand = new Random();
-		int randomNum = rand.nextInt(highnum - lownum + 1) + lownum;
-		return randomNum;
+		return lownum + (int)(rand.nextDouble() * ((highnum - lownum) + 1));
 	}
 }
